0c9a62dc9a4513bdd4c163db487889e5b0b2f416
Wayne Taylor <wayne@kryptos.co.uk>
2022-12-12 23:49:24 +0000

    init commit

---
+++ day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("Day 1");
+
+// path to data file
+string filename = @"C:\Users\wtt\source\repos\aoc2022\day1\data.txt";
+
+// read data into a list.
+List<string> allLinesText = File.ReadAllLines(filename).ToList();
+List<int> elf = new List<int>();
+
+int total = 0;
+
+foreach (var item in allLinesText)
+{
+    // look for gap
+    if (item != "")
+    {   
+        // total as we go
+        total += Int32.Parse(item);
+    }
+    else
+    {
+        // if gap found, that's an elf, save the elf and total
+        elf.Add(total);
+        total = 0;
+    }
+}
+
+// order in descending order and grab the top3
+var result = elf.OrderByDescending(e => e).Take(3);
+
+// find the elf with the highest and output to console
+Console.WriteLine("The greedy elf has: " + elf.Max(m => m));
+Console.WriteLine("Top 3 elfs total: " + result.Sum()); //sum top 3
+Console.ReadLine();
---
+++ day2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Day 2");
+// path to data file
+string filename = @"C:\Users\wtt\source\repos\aoc2022\day2\data.txt";
+
+// read data into a list.
+List<string> allLinesText = File.ReadAllLines(filename).ToList();
+
+// 1 for Rock, 2 for Paper, and 3 for Scissors
+
+// elf
+// A for Rock, B for Paper, and C for Scissors
+
+// me
+// X for Rock, Y for Paper, and Z for Scissors
+
+foreach (var item in allLinesText)
+{
+    Console.WriteLine(item);
+}
